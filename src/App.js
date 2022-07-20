adc7c3133042d9e365cbc3e5d5f7c58cfdd88f6e
BulatGim <bulat.gimaletdinov.01@gmail.com>
2022-07-20 15:17:00 +0300

    header done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import "./App.scss"
+import {Route, Routes} from "react-router-dom";
 import Main from "./pages/Main/Main";
+import Header from "./molecules/header/header";
+
+
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-        </Routes>
-      </BrowserRouter>
+          <div className="App">
+              <Header/>
+              <Routes>
+                  <Route path="/" element={<Main />}></Route>
+              </Routes>
+          </div>
   );
 }
 
