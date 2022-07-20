e40837c8c46a3c73738dfd987f4d197ce400b8ff
Vadim <vadik.kortkov@yandex.ru>
2022-07-20 13:45:23 +0300

    OurClients add

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Footer from '../../organisms/Footer/Footer'
 import Investments from '../../organisms/Investments/Investments';
+import OurClients from '../../organisms/OurClients/OurClients';
 import Projects from '../../organisms/Projects/Projects';
 import "./Main.scss"
 
 const Main = () => {
     return (
         <div className="main">
+            
             <Investments/>
             <Projects/>
+            <OurClients/>
             <Footer/>
         </div>
     );
