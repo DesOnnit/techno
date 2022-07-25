81e9c3f555bebd4278137eb48b4d566f1a504b02
Vadim Korotkov <vadik.kortkov@yandex.ru>
2022-07-25 12:33:03 +0300

    modal

--- src/atoms/Link/Link.js
+++ src/atoms/Link/Link.js
@@ -3,7 +3,7 @@ import './Link.scss';
 export default function LinkMenu(props) {
     return (
         <div className="link">
-            <NavLink className={({ isActive }) => (isActive && props.active ? "link__text link__text_active" : "link__text")} to={props.item.link} onClick={()=>window.scroll(0,0)}>
+            <NavLink className={({ isActive }) => (isActive && props.active ? "link__text link__text_active" : "link__text")} to={props.item.link} onClick={()=>window.scroll(0, 0)}>
                 <h5>{props.item.title}</h5>
             </NavLink>
         </div>
